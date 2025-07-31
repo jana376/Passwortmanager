e72179d5521802e27c435be1bb562941725053ae
Jana <jana@slavnic.com>
2025-07-31 08:42:21 +0200

    Merge veränderungen

--- src/main/java/ch/css/inexkasso/Main.java
+++ src/main/java/ch/css/inexkasso/Main.java
@@ -14,18 +14,15 @@ import static ch.css.inexkasso.SafeFunction.*;
 public class Main {
 
     public static void main(String[] args) throws SQLException {
+        Masterpassword masterpassword = new Masterpassword();
+        createTableIfNotExists();
+
         Scanner scanner = new Scanner(System.in);
-        Masterpassword mp = new Masterpassword();
-
-        mp.createTableIfNotExists();
-        mp.handleMasterPassword(scanner);
-
-        if (mp.getCurrentUsername() != null) {
-            mp.savePassword("Google", "meinPasswort123", "www.google.com");
-            mp.showPasswordsForUser();
-        }
+        masterpassword.handleMasterPassword(scanner);
+        help();
+        askWhatUserWantsToDo(scanner);
+        scanner.close();
     }
-
 
     private static void askWhatUserWantsToDo(Scanner scanner) {
         String userInput;
