eab8e86311fd7f27b9c791d2c3868583c29a7e08
Jana <jana@slavnic.com>
2025-08-07 09:49:16 +0200

    programm läuft wieder einigermassen, cmd wird noch nicht ausgegeben

--- src/main/java/ch/css/inexkasso/Masterpassword.java
+++ src/main/java/ch/css/inexkasso/Masterpassword.java
@@ -23,7 +23,7 @@ public class Masterpassword {
     }
 
     public int getNextMasterpasswordId() throws SQLException {
-        String sql = "SELECT MAX(MasterpasswordId) FROM Masterpassword";
+        String sql = "SELECT MasterpasswordId FROM MasterPassword";
         try (Connection conn = DriverManager.getConnection(URL);
              PreparedStatement stmt = conn.prepareStatement(sql);
              ResultSet rs = stmt.executeQuery()) {
